1c5fb521704e6927030f3dc00c8cf8fabb82f6c3
Jonathan Pelletier <maximusmagnusjopela@gmail.com>
2015-10-29 18:50:50 +0000

    Adding full implementation

--- guesslang/guesslang.py
+++ guesslang/guesslang.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
+from langdetect import detect
+
+def main():
+    """
+    guesslang entry point
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+            'text',
+            help='the text for which to guess the language')
+
+    args = parser.parse_args()
+
+    print(language(args.text))
+    return
+
+def language(txt):
+    """ returns the language of the txt """
+
+    lang = None
+    try:
+        lang = detect(txt)
+    except:
+        pass
+
+    return lang
+
+
+if __name__ == '__main__':
+    main()
